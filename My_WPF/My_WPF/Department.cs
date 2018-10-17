71b2b321c9dfbc99158cd5b667023ca360950540
iidyachenko <iidyachenko5@gmail.com>
2018-10-17 18:16:30 +0300

    Добавление департаментов

---
+++ My_WPF/My_WPF/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_WPF
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $" ID: {Id}\n Департаммент: {Name}";
+        }
+    }
+}
